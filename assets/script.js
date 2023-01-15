3dafcdca395f1242c90a997da704d9f7b8178b86
TJ Momon <tjmomon@TJs-MacBook-Air.attlocal.net>
2023-01-14 23:14:35 -0600

    added script to save user score to local storage

--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,20 @@ var timerInterval;
 var secondsLeft = 50;
 var quizSection = document.querySelector(".quiz-questions");
 var correctIndex = 0;
+var validate = document.querySelector("#validateAnswer");
+var submitButton = document.querySelector(".submit-btn");
+var viewScores = document.querySelector("#highscore");
+var highScorePage = document.querySelector(".highscores-card");
+// var highScoreSection = document.querySelector(".highscores-section");
+var nameEntry = document.querySelector(".high-score");
+var answer1 = document.querySelector(".answer1");
+var answer2 = document.querySelector(".answer2");
+var answer3 = document.querySelector(".answer3");
+var answer4 = document.querySelector(".answer4");   
+var question = document.querySelector(".question");
+
 
 // Questions Array
-var questionsArray = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10]
 var question1 = "What is JavaScript?"
 var question2 = "What is the main difference between Java and JavScript?"
 var question3 = "What are the JavaScript data types?"
@@ -17,72 +28,72 @@ var question7 = "Which syntax is used for single-line comments in JavaScript?"
 var question8 = "What is the difference between ViewState and SessionState?"
 var question9 = "What does the '===' operator do?"
 var question10 = "What are all of the looping structures in JavaScript?"
+var questionsArray = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10]
 
 // Answer Array
-var answersArray = [quizChoice1, quizChoice2, quizChoice3, quizChoice4, quizChoice5, quizChoice6, quizChoice7, quizChoice8, quizChoice9, quizChoice10]
 var quizChoice1 = {
     choice1: "Nothing but dark magic from the devil box",
     choice2: "JavaScript is a compiled language used to make the website interactive",
     choice3: "JavaScript is a powerful client-side scripting language used mainly for enhancing the interaction of a user with the webpage",
     choice4: "None of the above",
-}
+};
 var quizChoice2 = {
     choice1: "JavaScript is a complete programming language, while Java is a coded program that can be introduced to HTML pages",
     choice2: "Java is a complete programming language, while javaScript is a coded program that can be introduced to HTML pages",
     choice3: "Java is what I get from Starbucks, and javaScript is what happens when I drink coffee while coding",
     choice4: "Java and javaScript are the same thing",
-}
+};
 var quizChoice3 = {
     choice1: "Number, String, Boolean, Object, Undefined",
     choice2: "String, DIV, Null, Boolean, Integer",
     choice3: "Unknown, 404, localStorage, CSS, .png",
     choice4: "None of the above",
-}
+};
 var quizChoice4 = {
     choice1: "NetGear",
     choice2: "NetScape",
     choice3: "NetFlix",
     choice4: "Net-Fishing",
-}
+};
 var quizChoice5 = {
     choice1: "A box that allows the user to diagnose potential issues in their code",
     choice2: "An annoying pop-up trying to sell you something",
     choice3: "A box that allows the user to enter input by providing a textbox",
     choice4: "All of the above",
-}
+};
 var quizChoice6 = {
     choice1: "'This' refers to the fact that 'This' is confusing",
     choice2: "'This' keyword refers to the object from a different div",
     choice3: "'This' refers to the specific function that you are working in",
     choice4: "'This' keyword refers to the object from where it is called",
-}
+};
 var quizChoice7 = {
     choice1: "-/",
     choice2: "//",
     choice3: "/*",
     choice4: "comment();",
-}
+};
 var quizChoice8 = {
     choice1: "'ViewState' is specific to a page in session, while 'SessionState' is specific to user-specific data that can be accessed accross all web application pages",
     choice2: "'ViewState is the current part of the web application that you are viewing, while 'SessionState' is the current state of your web page",
     choice3: "'ViewState' is what I did in 5th grade Geography, and 'SessionState' is what happens every election year",
     choice4: "None of the above",
-}
+};
 var quizChoice9 = {
     choice1: "Relatively compares two operands that must have the same value with conversion",
     choice2: "Creates Train Tracks",
     choice3: "Strictly compares three operands that must have the same value without conversion",
     choice4: "Strictly compares two operands that must have the same value without conversion",
-}
+};
 var quizChoice10 = {
     choice1: "'For loops', 'While loops', 'Do-While loops'",
     choice2: "'Fruit', 'hula', 'basketball'",
     choice3: "'else', 'else if', 'function'",
     choice4: "None of the above",
-}
+};
+var answersArray = [quizChoice1, quizChoice2, quizChoice3, quizChoice4, quizChoice5, quizChoice6, quizChoice7, quizChoice8, quizChoice9, quizChoice10]
 
 // Correct Answer Array
-var correctArray = [correct1, correct2, correct3, correct4, correct5, correct6, correct7, correct8, correct9, correct10]
 var correct1 = quizChoice1.choice3;
 var correct2 = quizChoice2.choice2;
 var correct3 = quizChoice3.choice1;
@@ -93,6 +104,7 @@ var correct7 = quizChoice7.choice2;
 var correct8 = quizChoice8.choice1;
 var correct9 = quizChoice9.choice3;
 var correct10 = quizChoice10.choice1;
+var correctArray = [correct1, correct2, correct3, correct4, correct5, correct6, correct7, correct8, correct9, correct10]
 
 // Starts the quiz
 startQuizBtn.addEventListener("click", startTimer);
@@ -109,25 +121,20 @@ function startTimer() {
         }
     }, 1000);
     return timerInterval;
-}
+};
 
 // Function to hide intro screen
 function hideIntro() {
     document.querySelector(".card-main").style.display = "none";
     quizSection.style.display = "block";
     nextQuestion();
-}
+};
 
 // Function to populate next question
 function nextQuestion(){
-    var answer1 = document.querySelector(".answer1");
-    var answer2 = document.querySelector(".answer2");
-    var answer3 = document.querySelector(".answer3");
-    var answer4 = document.querySelector(".answer4");   
-    var question = document.querySelector(".question");
     if(correctIndex === questionsArray.length) {
         setTimeout (function() {quizSection.style.display = "none";
-        initialsSection.style.display = "inline";
+        nameEntry.style.display = "inline";
     },1000);
         setTimeout(function(){clearInterval(timerInterval)},1000);
     } else {
@@ -137,13 +144,12 @@ function nextQuestion(){
         answer3.textContent = answersArray[correctIndex].choice3;
         answer4.textContent = answersArray[correctIndex].choice4;
     }
-}
+};
 
 // Validates user answer, then adds or subtracts time based on validation
 quizSection.addEventListener("click", correctAnswer)
 
-function correctAnswer(event){
-    var validate = document.querySelector("#validateAnswer");
+function correctAnswer(event) {
     if(event.target.matches(".btn-question")){
         var chosenAnswer = event.target.textContent;
         validate.textContent = "";
@@ -163,3 +169,30 @@ function correctAnswer(event){
     }
     return secondsLeft;
 };
+
+quizSection.addEventListener("click", function(event){
+    if (event.target.matches(".btn-question")) {
+        nextQuestion();
+    }
+});
+
+// Function to set user input to local storage
+function newUser() {
+    var userInitial = document.querySelector("#initials").value;
+    if (userInitial === "") {
+        userInitial = "anonymous";
+    }
+    localStorage.setItem(userInitial, secondsLeft);
+    document.querySelector(".user-scores").textContent = " ";
+    var a = document.createElement("a");
+    a.textContent = userInitial + ": " + secondsLeft;
+    document.querySelector(".user-scores").appendChild(p);
+};
+
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    newUser();
+        initialsSection.style.display = "none";
+        document.querySelector(".highscores-card").style.display = "block";
+        document.querySelector(".user-scores").style.display = "block";
+});
